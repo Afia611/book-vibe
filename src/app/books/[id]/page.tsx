54d1b2ae9023eaf12683dfa611fe9acde67d0799
mahiramer <e0333007@u.nus.edu>
2026-09-24 15:49:24 +0600

    prepare project for deployment

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -11,9 +11,8 @@ interface IBookDetailsPageProps{
 }
 
 const getBooks = async (): Promise< IBook[]> => {
-  const response = await fetch(
-    "http://localhost:3000/booksData.json"
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`,
+ );
   const data: IBook[] = await response.json();
   return data;
 };
@@ -36,7 +35,7 @@ return (
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
 
         {/* LEFT SIDE - BOOK IMAGE */}
-        <div className="bg-base-200 rounded-2xl p-10 flex items-center justify-center min-h-[500px]">
+        <div className="bg-base-200 rounded-2xl p-10 flex items-center justify-center min-h-500px">
 
           <Image
             src={book.image}
